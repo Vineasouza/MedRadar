fdec76447ed9ea574d6b350b293d970a665e16a4
Vinicius Souza <vineasouza33@hotmail.com>
2020-08-18 13:01:00 -0300

    add mongoose schema

---
+++ backend/src/models/Med.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const PointSchema = require('./utils/PointSchema');
+
+const MedSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        require: true,
+    },
+    especialidade:{
+        type: String,
+        require: true,
+    },
+    convenio: {
+        type: [String],
+        require: true
+    },
+    uf: {
+        type: String,
+        require: true
+    },
+    cidade: {
+        type: String,
+        require: true
+    },
+    endereço: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        validate: {
+          validator: function(v) {
+            return /\(?\d{2,3}\)?\d{4,5}-?\d{4}/.test(v);
+          },
+          message: '{VALUE} is not a valid phone number!'
+        },
+        required: [true, 'User phone number required']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    }, 
+    location: {
+        type: PointSchema,
+        index: '2dsphere'
+    },
+});
+
+module.exports = mongoose.model('Med', DevSchema);
