6c5b848e99d93ada0781dea7d06168acec7c918c
Vinicius Souza <vineasouza33@hotmail.com>
2020-08-25 20:55:46 -0300

    add page AddDoctor - initial changes

--- frontend/src/pages/addDoctor/index.jsx
+++ frontend/src/pages/addDoctor/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import logo from '../../assets/images/simple-only-logo.png';
+import mainDoctor from '../../assets/images/Online Doctor-bro.png';
+import './styles.css';
 
 
 function AddDoctor() {
     return (
-        <div>
-            <h1>Page add doctor</h1>
-        </div>
+        <main>
+            <header className="main-header">
+                <img src={logo} className="doctor-logo" alt="logo" />
+                <img src={mainDoctor} className="main-doctor" alt="doctor" />
+            </header>
+            <form className="forms-block">
+                <label className="nome">
+                Nome
+                <input type="text" name="nome" placeholder="Vinicius Souza"/>
+                </label>
+                <label className="especialidade">
+                Especialidade
+                <input type="text" name="especialidade" placeholder="Neurologista"/>
+                </label>
+            </form>
+        </main>
     );
 }
 
