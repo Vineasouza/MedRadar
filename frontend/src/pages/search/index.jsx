ffa2eeb1e88d1cd37492aa12e56bedb869525b69
arthur-andraade <arthurandradenf@gmail.com>
2020-08-26 04:13:43 -0300

    making header the page search

--- frontend/src/pages/search/index.jsx
+++ frontend/src/pages/search/index.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 import './styles.css';
 
+import logo from '../../assets/images/simple-only-logo.png';
+import healthTeam from '../../assets/images/health-team-bro.png';
+
 function Search() {
     return (
-        <div>
-            <h1>Page Search</h1>
+        <div id="search-page">
+            <header>
+                <section className="search-intro">
+                    <img src={logo}></img>
+                    <img src={healthTeam}></img>
+                </section>
+                <section className="search-actions">
+                    <button>Pesquisar</button>
+                    <button>Filtrar</button>
+                </section>
+            </header>
         </div>
     )
 }
