8350102f523ad0f266794b9af34633fa3e26d2fd
arthur-andraade <arthurandradenf@gmail.com>
2020-08-26 15:38:21 -0300

    Making the layout the main of search

--- frontend/src/pages/search/index.jsx
+++ frontend/src/pages/search/index.jsx
@@ -10,14 +10,25 @@ function Search() {
         <div id="search-page">
             <header>
                 <section className="search-intro">
-                    <img src={logo}></img>
-                    <img src={healthTeam}></img>
+                    <img src={logo} alt="logo do MedRadar"></img>
+                    <img src={healthTeam} alt="grupo de profissionais"></img>
                 </section>
                 <section className="search-actions">
                     <button>Pesquisar</button>
                     <button>Filtrar</button>
                 </section>
             </header>
+
+            <main id="search-main">
+                <section className="search-result">
+                    <div>
+
+                    </div>
+                </section>
+                <section className="search-map">
+
+                </section>
+            </main>
         </div>
     )
 }
