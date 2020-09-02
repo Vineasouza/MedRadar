9a6a32f2f676d352f3a59305aa3e91726a209e29
Vinicius Souza <vineasouza33@hotmail.com>
2020-09-02 16:13:21 -0300

    add changes at forms to connect to backend

--- frontend/src/pages/addDoctor/index.jsx
+++ frontend/src/pages/addDoctor/index.jsx
@@ -1,38 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/images/simple-only-logo.png';
 import mainDoctor from '../../assets/images/Online Doctor-bro.png';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
 
-function AddDoctor() {
+function AddDoctor({ onSubmit }) {
+    const [nome, setNome] = useState('');
+    const [especialidade, setEspecialidade] = useState('');
+    const [convenio, setConvenio] = useState('');
+    const [uf, setUf] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        await onSubmit({
+            nome,
+            especialidade,
+            convenio,
+            uf,
+            cidade,
+            endereco,
+            telefone,
+            email,
+        });
+
+        // setGithubUsername('');
+        // setTechs('');
+    }
+
     return (
         <main>
             <header className="main-header">
                 <img src={logo} className="doctor-logo" alt="logo" />
                 <img src={mainDoctor} className="main-doctor" alt="doctor" />
             </header>
-            <form className="forms-block">
+            <form className="forms-block" onSubmit={handleSubmit}>
                 <label className="nome">Nome</label>
                 <input 
                     type="text" 
                     name="nome" 
+                    id="nome"
+                    required
                     placeholder="Nome completo"
+                    value={nome} 
+                    onChange={e => setNome(e.target.value)} 
                 />
                 
                 <label className="especialidade">Especialidade</label>
                 <input 
                     type="text" 
                     name="especialidade" 
+                    id="especialidade"
+                    required 
                     placeholder="Especialidade"
+                    value={especialidade} 
+                    onChange={e => setEspecialidade(e.target.value)} 
                 />
 
                 <label className="convenio">Convênio</label>
-                <input type="text" name="convenio" placeholder="sulAmerica, Unimed"/>
+                <input 
+                    type="text" 
+                    name="convenio" 
+                    id="convenio"
+                    required 
+                    placeholder="sulAmerica, Unimed"
+                    value={convenio} 
+                    onChange={e => setConvenio(e.target.value)} 
+                />
+
                 <div className="forms-city">
                     <label className="uf">
                     UF
-                    <select name="estados-brasil">
+                    <select 
+                        name="estados-brasil"
+                        id="uf"
+                        required
+                        value={uf} 
+                        onChange={e => setUf(e.target.value)} 
+                    >
                         <option value="blank">  </option>
                         <option value="AC">AC</option>
                         <option value="AL">AL</option>
@@ -66,8 +116,12 @@ function AddDoctor() {
                     <label className="cidade">Cidade
                     <input 
                         type="text" 
-                        name="cidade" 
+                        name="cidade"
+                        id="cidade"
+                        required 
                         placeholder="Nome da Cidade"
+                        value={cidade} 
+                        onChange={e => setCidade(e.target.value)} 
                     />
                     </label>
                 </div>
@@ -75,21 +129,37 @@ function AddDoctor() {
                 <input 
                     type="text" 
                     name="endereco" 
+                    id="endereco" 
+                    required
                     placeholder="Rua/Av., N°, Comp, Bairro"
+                    value={endereco} 
+                    onChange={e => setEndereco(e.target.value)} 
                 />
                 
                 <label className="telefone">Telefone</label>
                 <input 
                     type="number" 
-                    name="telefone" 
+                    name="telefone"
+                    id="telefone"
+                    required 
+                    pattern="\(?[0]?\d{2}\)?[9]?\d{4}-?\d{4}"
+                    title="Invalid input"
                     placeholder="(xxx)xxxxx-xxxx"
+                    value={telefone} 
+                    onChange={e => setTelefone(e.target.value)} 
                 />
                 
                 <label className="email">E-mail</label>
                 <input 
                     type="email" 
                     name="email" 
+                    id="email"
+                    required 
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                    title="Invalid input"
                     placeholder="email@email.com.br"
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)} 
                 />
                 
             </form>
