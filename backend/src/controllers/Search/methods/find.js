528127b1f2c204e79344a61b7b006b9c5432811a
arthur-andraade <arthurandradenf@gmail.com>
2020-10-15 17:36:34 -0300

    Clean the method find

--- backend/src/controllers/Search/methods/find.js
+++ backend/src/controllers/Search/methods/find.js
@@ -6,78 +6,65 @@ module.exports = async function find(request, response) {
   }
 
   let resultSearch;
-  const { latitude, longitude, specialty, city } = request.query;
+  const {
+    latitude,
+    longitude,
+    specialty,
+    city,
+    age,
+    healthPlan,
+  } = request.query;
 
-    if (Object.keys(request.query).length === 0) {
-        return response.status(406).send();
-    }
+  let query = {};
 
-    console.log(request.query);
-    let resultSearch;
-    const {
-        latitude,
-        longitude,
-        specialty,
-        city,
-        age,
-        healthPlan
-    } = request.query;
+  if (specialty != undefined) {
+    query.especialidade = specialty;
+  }
+  if (city != undefined) {
+    query.cidade = city;
+  }
+  if (age != undefined) {
+    query.idade = { $gte: age };
+  }
+  if (healthPlan != undefined) {
+    query.convenio = healthPlan;
+  }
 
-    let query = {};
-
-    if (specialty != undefined) {
-        query.especialidade = specialty;
-    }
-    if (city != undefined) {
-        query.cidade = city
-    }
-    if (age != undefined) {
-        query.idade = { $gte: age };
-    }
-    if (healthPlan != undefined) {
-        query.convenio = healthPlan;
-    }
-
-    /* 
+  /* 
         Option where the user are looking for
         doctors only by Radius
     */
-    let radius;
-    if (request.query.radius === undefined) radius = 10; // Default to radius!
-    else radius = request.query.radius
+  let radius;
+  if (request.query.radius === undefined) radius = 10;
+  // Default to radius!
+  else radius = request.query.radius;
 
-    if (Object.keys(query).length === 0) {
-        resultSearch = await Med.find({
-            location: {
-                $near: {
-                    $geometry: {
-                        type: 'Point',
-                        coordinates: [longitude, latitude],
-                    },
-                    $maxDistance: radius * 1000, // Converting from km to m
-                },
-            }
-        });
-
-        /*  Option where the user are looking for
-            doctors only by Radius, city or specialty
-        */    
-    } else {
-        console.log(query);
-        resultSearch = await Med.find({
-            ...query
-            ,
-            location: {
-                $near: {
-                    $geometry: {
-                        type: 'Point',
-                        coordinates: [longitude, latitude],
-                    },
-                    $maxDistance: radius * 1000,
-                },
-            }
-        });
-    }
+  if (Object.keys(query).length === 0) {
+    resultSearch = await Med.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: radius * 1000, // Converting from km to m
+        },
+      },
+    });
+  } else {
+    resultSearch = await Med.find({
+      ...query,
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: radius * 1000,
+        },
+      },
+    });
+  }
 
   if (resultSearch.length === 0) {
     return response.status(404).json({
