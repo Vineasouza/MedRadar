57dca486084ad82def62e5669d2ca409412638b4
Vinicius Souza <vineasouza33@hotmail.com>
2020-10-20 13:44:12 -0300

    upgrade img size at multer

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -38,7 +38,7 @@ module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
   storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
-    fileSize: 2 * 1024 * 1024,
+    fileSize: 10 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
