a036f8af25016356586658f3ef1a43a38f894fb2
Vinicius Souza <vineasouza33@hotmail.com>
2020-11-02 19:14:22 -0300

    add a 404 error on page not found

--- frontend/src/pages/search/components/notFound/index.jsx
+++ frontend/src/pages/search/components/notFound/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { HiOutlineEmojiSad } from "react-icons/hi"
+import Error from '../../../../assets/images/404error.png'
 
 import "./styles.css";
 
 function NotFound() {
     return (
-        <div id="not-found-container">
-            <h1>Desculpa, nenhum médico encontrado  <HiOutlineEmojiSad /></h1>
+        <div id="container">
+            <img src={Error} alt="logo" />
+            <div id="not-found-container">
+                <h1>Desculpa, nenhum médico encontrado  <HiOutlineEmojiSad /></h1>
+            </div>
         </div>
     )
 }
